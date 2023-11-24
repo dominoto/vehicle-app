9fcb8b8a75d96559be7f7582da845c7a154f4b63
dominoto <9913835+dominoto@users.noreply.github.com>
2023-11-24 19:26:40 +0100

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    // "plugin:react/jsx-runtime",
+    // "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+  },
+};
