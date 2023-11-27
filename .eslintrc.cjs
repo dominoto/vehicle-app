5d003084f7c8b8f960168c916ccc3948d6887c82
dominoto <9913835+dominoto@users.noreply.github.com>
2023-11-27 19:17:57 +0100

    Implement routing with React Router

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    // "plugin:react/jsx-runtime",
-    // "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: {
-    react: { version: "18.2" },
-  },
-  plugins: ["react", "prettier"],
-  rules: {
-    "react/react-in-jsx-scope": 0,
-  },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        // "plugin:react/jsx-runtime",
+        // "plugin:react-hooks/recommended",
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: {
+        react: { version: '18.2' },
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 0,
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './Pages/Root';
+import ErrorPage from './Pages/ErrorPage';
+import VehicleEditPage from './Pages/VehicleEditPage';
+import VehicleCreatePage from './Pages/VehicleCreatePage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: 'vehicles/:id/edit',
+                element: <VehicleEditPage />,
+            },
+            {
+                path: 'vehicles/create',
+                element: <VehicleCreatePage />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
